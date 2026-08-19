acf8d2901577c7de36e69ac56247ec90daa68d3b
Soulkau <156959788+Soulkau@users.noreply.github.com>
2026-08-19 22:31:30 +0300

    feat(mushclim): send metrics and exhaust state

--- src/exhaust.rs
+++ src/exhaust.rs
@@ -3,43 +3,63 @@ use core::fmt::Write;
 use driverse::relay::Relay;
 use esp_hal::gpio::Output;
 use heapless::String;
+use ivy::mqtt::MqttHandle;
+use serde::{Deserialize, Serialize};
 
 use crate::{MushclimConfig, timer::CycleTimer};
 
 pub struct ExhaustManager<'a> {
     pub exhaust: Relay<Output<'a>>,
     pub timer: CycleTimer,
+    pub handle: MqttHandle<255>,
 }
 
 impl<'a> ExhaustManager<'a> {
-    pub fn new(exhaust: Relay<Output<'a>>, config: &MushclimConfig) -> Self {
+    pub fn new(
+        exhaust: Relay<Output<'a>>,
+        config: &MushclimConfig,
+        handle: MqttHandle<255>,
+    ) -> Self {
         Self {
             exhaust,
             timer: CycleTimer::new(config.exhaust_duty_interval, config.exhaust_duty),
+            handle,
         }
     }
 
     pub async fn tick(&mut self) -> bool {
         let should_be_on = self.timer.tick();
-
+        let is_on = self.exhaust.is_on();
         if should_be_on {
-            if !self.exhaust.is_on() {
+            if !is_on {
+                self.switch(true).await;
                 tracing::info!("[Exhaust]: Turned on");
             }
-            self.exhaust.on();
         } else {
-            if self.exhaust.is_on() {
+            if is_on {
+                self.switch(false).await;
                 tracing::info!("[Exhaust]: Turned off");
             }
-            self.exhaust.off();
         }
         should_be_on
     }
 
-    pub fn format_state(&self) -> String<16> {
+    pub async fn switch(&mut self, on: bool) {
+        self.handle
+            .publish("mushclim/fan", ExhaustState { on })
+            .await;
+        if on {
+            self.exhaust.on();
+        } else {
+            self.exhaust.off();
+        }
+    }
+
+    pub fn log_state(&self) -> String<16> {
         let mut string = String::new();
         let _ = string.write_str("Fan: ");
         let (started, time) = self.timer.time_until_change();
+
         if started {
             let _ = string.write_str("off");
         } else {
@@ -60,4 +80,9 @@ impl<'a> ExhaustManager<'a> {
     pub fn reset(&mut self) {
         self.exhaust.off();
     }
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct ExhaustState {
+    on: bool,
 }
