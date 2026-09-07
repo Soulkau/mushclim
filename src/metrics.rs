c899a65cfa5ec062d3d4301056cb3206e07f4e6c
Soulkau <156959788+Soulkau@users.noreply.github.com>
2026-09-07 20:59:20 +0300

    refactor(metrics): add error logging, fix minor logic issues

--- src/metrics.rs
+++ src/metrics.rs
@@ -6,6 +6,9 @@ use embassy_time::Instant;
 use heapless::HistoryBuf;
 use serde::{Deserialize, Serialize};
 
+const MINIMUM_METRIC_AGE_SECONDS: u64 = 360;
+
+/// Immutable, calculated from `UnfinishedStat`
 #[derive(Clone, Copy, Deserialize, Serialize)]
 pub(crate) struct Stat {
     pub min: f32,
@@ -13,6 +16,15 @@ pub(crate) struct Stat {
     pub avg: f32,
 }
 
+#[derive(Clone, Copy, Deserialize, Serialize)]
+pub(crate) struct Metric {
+    pub uptime_secs: u64, // when this period *ended*, relative to boot
+    pub temp: Stat,
+    pub humidity: Stat,
+    pub co2: Stat,
+    pub samples: u32,
+}
+/// Structure that accamulates values, and calculates finished `Stat`
 #[derive(Default)]
 struct UnfinishedStat {
     min: f32,
@@ -35,16 +47,7 @@ impl UnfinishedStat {
         }
     }
 }
-
-#[derive(Clone, Copy, Deserialize, Serialize)]
-pub(crate) struct Metric {
-    pub uptime_secs: u64, // when this period *ended*, relative to boot
-    pub temp: Stat,
-    pub humidity: Stat,
-    pub co2: Stat,
-    pub samples: u32,
-}
-
+/// Metric that has to be finished, bakes into immutable `Metric` after finish
 struct UnfinishedMetric {
     started_at: Instant,
     temp_stat: UnfinishedStat,
@@ -63,14 +66,14 @@ impl UnfinishedMetric {
             samples: 0,
         }
     }
-
+    /// Updates all stat by measurments
     fn record(&mut self, measurement: Measurement) {
         self.temp_stat.update(measurement.temperature_c);
         self.humidity_stat.update(measurement.humidity_pct);
         self.co2_stat.update(measurement.co2_ppm as f32);
         self.samples += 1;
     }
-
+    /// Creates finalized metric
     fn finish(&self) -> Metric {
         Metric {
             uptime_secs: Instant::now().as_secs(),
@@ -82,6 +85,8 @@ impl UnfinishedMetric {
     }
 }
 
+///Manages metric updates and sends them via mqtt
+///Has cache for last 10 metrics.
 pub(crate) struct MetricManager {
     unfinished: UnfinishedMetric,
     cache: HistoryBuf<Metric, METRIC_SNAPSHOT_AMOUNT>,
@@ -96,7 +101,8 @@ impl MetricManager {
             timer: DueTimer::new(config.metric_send_period),
         }
     }
-
+    /// Feeds new measurements to currently unfinished metric.
+    /// Sends metrics via mqtt every `config.metric_send_period`
     pub async fn feed(&mut self, measurements: Measurement, handle: &MushclimMqttHandle) {
         self.unfinished.record(measurements);
 
@@ -104,24 +110,28 @@ impl MetricManager {
             return;
         }
 
-        if self.unfinished.started_at.elapsed().as_secs() >= 1 {
-            let finished = self.unfinished.finish();
-            let _ = handle.publish("mushclim/metrics", finished).await;
-            self.cache.write(finished);
-            self.unfinished = UnfinishedMetric::new();
+        //In case time somehow runs out before metric actually has 'usefull' information in it
+        if self.unfinished.started_at.elapsed().as_secs() < MINIMUM_METRIC_AGE_SECONDS {
+            tracing::warn!("[MetricManager] Metric is too new to send it, reseting it");
+            self.reset_metric();
+            return;
         }
+
+        let finished = self.unfinished.finish();
+        if let Err(e) = handle.publish("mushclim/metrics", finished).await {
+            tracing::error!("[MetricManager] Failed to publish metrics, {}", e);
+        };
+        self.cache.write(finished);
+        self.reset_metric();
     }
 
-    /// Most recent N snapshots, newest first.
-    pub fn recent(&self, n: usize) -> impl Iterator<Item = &Metric> {
-        self.cache.oldest_ordered().rev().take(n)
+    fn reset_metric(&mut self) {
+        self.unfinished = UnfinishedMetric::new();
     }
 
-    pub fn all(&self) -> impl Iterator<Item = &Metric> {
-        self.cache.oldest_ordered()
-    }
-
+    /// Updates config, resets unfinished metric
     pub fn force_config(&mut self, config: &MushclimConfig) {
         self.timer = DueTimer::new(config.metric_send_period);
+        self.reset_metric();
     }
 }
