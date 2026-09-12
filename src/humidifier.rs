83aa7a1fd473ad18f63590250f75b11531adf795
Soulkau <156959788+Soulkau@users.noreply.github.com>
2026-09-12 16:31:02 +0300

    feat(humidifer): add cycle mode

--- src/humidifier.rs
+++ src/humidifier.rs
@@ -1,6 +1,7 @@
 use core::ops::RangeInclusive;
 
 use driverse::relay::Relay;
+use embassy_time::Duration;
 use embedded_hal::digital::OutputPin;
 
 use crate::{
@@ -26,31 +27,55 @@ impl From<&MushclimConfigDto> for HumidifierConfig {
     }
 }
 
+pub enum HumidifierMode {
+    Normal,
+    Cycled(CycleTimer),
+}
+
 pub(crate) struct Humidifier<P: OutputPin> {
     switch: Relay<P>,
-    treshold: RangeInclusive<u16>,
+    config: HumidifierConfig,
+    mode: HumidifierMode,
 }
 
 impl<P: OutputPin> Humidifier<P> {
-    pub fn new(switch: Relay<P>, config: &MushclimConfig) -> Self {
+    pub fn new(switch: Relay<P>, config: &HumidifierConfig) -> Self {
         Self {
             switch,
-            treshold: config.humidity_threshold.clone(),
+            config: config.clone(),
+            mode: HumidifierMode::Normal,
         }
     }
 
+    pub fn set_normal_mode(&mut self) {
+        tracing::info!("[Humidifier]: switching to normal mode");
+        self.mode = HumidifierMode::Normal;
+        self.turn_off();
+    }
+
+    pub fn set_cycled_mode(&mut self) {
+        tracing::info!("[Humidifier]: switching to cycled mode");
+        self.mode = HumidifierMode::Cycled(CycleTimer::new(
+            self.config.safe_humidity_duty_interval,
+            self.config.safe_humidity_duty,
+        ));
+        self.turn_off();
+    }
+
+    /// Normal-mode tick, uses live humidity measurements. No-op if currently in cycled mode.
     pub fn tick(&mut self, measurements: &Measurement, is_exhaust_on: bool) {
-        if is_exhaust_on {
-            if self.switch.is_on() {
-                tracing::info!("Humidifier: Paused, exhaust active.");
-            }
-            self.switch.off().ok();
+        if !matches!(self.mode, HumidifierMode::Normal) {
+            tracing::debug!("[Humidifier]: tick() called while in cycled mode, ignoring");
+            return;
+        }
+
+        if self.pause_for_exhaust(is_exhaust_on) {
             return;
         }
 
         let current_humidity = measurements.humidity_pct;
-        let low_bound = *self.treshold.start() as f32;
-        let comfort_bound = *self.treshold.end() as f32;
+        let low_bound = *self.config.humidity_threshold.start() as f32;
+        let comfort_bound = *self.config.humidity_threshold.end() as f32;
 
         if current_humidity <= low_bound {
             if !self.switch.is_on() {
@@ -73,12 +98,56 @@ impl<P: OutputPin> Humidifier<P> {
         }
     }
 
-    pub fn update_config(&mut self, config: &MushclimConfig) {
-        self.treshold = config.humidity_threshold.clone();
+    /// Cycled-mode tick, no sensor needed. No-op if currently in normal mode.
+    pub fn tick_cycled(&mut self, is_exhaust_on: bool) {
+        // Return if in wrong mode
+        if matches!(self.mode, HumidifierMode::Normal) {
+            tracing::debug!("[Humidifier]: tick_cycled() called while in normal mode, ignoring");
+            return;
+        }
+        // Ignore tick if exhaust is on.
+        if self.pause_for_exhaust(is_exhaust_on) {
+            if let HumidifierMode::Cycled(timer) = &mut self.mode {
+                timer.ingore_tick();
+            }
+            return;
+        }
+
+        let should_be_on = if let HumidifierMode::Cycled(timer) = &mut self.mode {
+            timer.tick()
+        } else {
+            false
+        };
+
+        if should_be_on && !self.switch.is_on() {
+            tracing::info!("[Humidifier]: cycled duty started, turning ON.");
+            self.switch.on().ok();
+        } else if !should_be_on && self.switch.is_on() {
+            tracing::info!("[Humidifier]: cycled duty ended, turning OFF.");
+            self.switch.off().ok();
+        }
     }
 
-    pub fn turn_on(&mut self) {
-        let _ = self.switch.on();
+    fn pause_for_exhaust(&mut self, is_exhaust_on: bool) -> bool {
+        if is_exhaust_on {
+            if self.switch.is_on() {
+                tracing::info!("[Humidifer]: Paused humidifer due to exhaust being on");
+            }
+            self.switch.off().ok();
+            return true;
+        }
+        false
+    }
+
+    pub fn force_config(&mut self, config: &MushclimConfig) {
+        self.config = config.humidifier.clone();
+        if matches!(self.mode, HumidifierMode::Cycled(_)) {
+            // duty/interval may have changed, rebuild the timer against the new config
+            self.mode = HumidifierMode::Cycled(CycleTimer::new(
+                self.config.safe_humidity_duty_interval,
+                self.config.safe_humidity_duty,
+            ));
+        }
     }
 
     pub fn turn_off(&mut self) {
